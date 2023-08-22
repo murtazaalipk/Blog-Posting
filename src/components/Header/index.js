2350f3df8df01a7d74f5e5e538653aafcdbc592b
Syed Murtaza Ali <125219980+SyedMurtazaAli-dev@users.noreply.github.com>
2023-08-22 18:20:57 +0500

    Create Header Component

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import Button from "../Button"
+export default function Header() {
+
+    return (
+        <>
+        <nav>
+            <div>
+                <Button text="Login" />
+                <Button text="Logout" />
+                <Button text="Profile" />
+
+            </div>
+        </nav>
+        </>
+    )
+
+}
