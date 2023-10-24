faa22030bc21397f00e07ded713d4adfe8e7419b
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2023-10-24 19:09:22 +0500

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,13 +1,23 @@
 import Blog from "@/components/blog";
 import Dashboard from "@/components/dashboard";
 import { getSession } from "next-auth/react"
-
+import data from '@/data/blogs.json';
 
 export default function SignUp() {
-  return (<div>
+  return (
+  <div>
     <Dashboard/>
-    <Blog/>
+    <div className='flex flex-col items-center ' >
+    
+    {(() => {
+            for (let i = 0 ; i <= data.length ; i++) {
+             <Blog data={data[i]}/>
+            }
+          })()}
+    <Blog data={data[0]}  />
+    <Blog data={data[1]}/> 
     </div>
+  </div>
   )
 };
 export async function getServerSideProps({ req }) {
