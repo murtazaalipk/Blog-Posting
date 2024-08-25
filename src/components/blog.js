8a6020883765fd42dab1b2405d34ea3a11900b9d
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2024-08-25 19:02:45 +0500

    fetch blogs in fronted

--- src/components/blog.js
+++ src/components/blog.js
@@ -1,36 +1,24 @@
 import Image from "next/image";
 import Pic from "../pics/profile.png";
-
+import { useState, useEffect } from "react";
 export default function Blog() {
-  const blogs = [
-    {
-      id:1,
-      Title: "How to make a website Using NextJs",
-      Name: "Syed Murtaza Ali",
-      designation: "Developer",
-      Blog: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-    {
-      id:2,
-      Title: "How to buy StarLink Internet ",
-      Name: "Elone Musk",
-      designation: "CEO of Tesla Motors",
-      Blog: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-    {
-      id:3,
-      Title: "How to buy StarLink Internet",
-      Name: "Elone Musk",
-      designation: "CEO of Tesla Motors",
-      Blog: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    },
-  ];
+  const [Blogs, setBlog] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await fetch("/api/getAllBlogs");
+      const blogs = await response.json();
+      setBlog(blogs.blogs);
+    };
+    fetchBlogs();
+  }, []);
+  console.log(Blogs);
 
   return (
     <div className="pl-40 pr-40 pb-10 pt-10">
       <div>
-        {blogs.map((blog , index) => (
-          <div key={blog.id} className=" border border-gray-300 m-5 rounded">
+        {Blogs.map((blog) => (
+          <div key={blog._id} className=" border border-gray-300 m-5 rounded">
             <div className=" flex gap-7 items-center p-5">
               <div className="flex m-5">
                 <Image
@@ -42,14 +30,14 @@ export default function Blog() {
                 />
               </div>
               <div className="m-5 flex flex-col">
-                <h1 className=" text-xl font-bold">{blog.Title}</h1>
+                <h1 className=" text-xl font-bold">{blog.title}</h1>
                 <p className=" text-gray-600 font-bold">
-                  {blog.Name} - {blog.designation}
+                  {blog.AuthorName} - {blog.AuthorDesignation}
                 </p>
               </div>
             </div>
             <div className="m-5">
-              <p className=" p-5 text-justify">{blog.Blog}</p>
+              <p className=" p-5 text-justify">{blog.content}</p>
             </div>
           </div>
         ))}
